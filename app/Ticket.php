9844e76a2da17941c129799f4d62be972ca4f914
sparshturkane <rajaturkane99@gmail.com>
2017-07-26 14:37:42 +0530

    displaying in comments

--- app/Ticket.php
+++ app/Ticket.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     protected $fillable = ['title', 'content', 'slug', 'status', 'user_id'];
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment', 'post_id');
+    }
 }
